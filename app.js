e2d49f0f4a4d718a839cdd8e3b79c1f2ebaed1c4
rkunkel <rkunkel@adaptivebiotech.com>
2015-09-04 13:59:12 -0700

    .

--- app.js
+++ app.js
@@ -18,6 +18,46 @@ var main = function() {
             left: '0px'},
             200);
     });
+
+    $('.dropdown-toggle').click(function () {
+        $('.dropdown-menu').toggle();    
+    });
+    
+    $('.arrow-next').click(function () {
+        var currentDot = $('.active-dot');
+        var currentSlide = $('.active-slide');
+        var nextDot = currentDot.next();
+        var nextSlide = currentSlide.next();
+        
+        if (nextSlide.length == 0) {
+            nextSlide = $('.slide').first();
+            nextDot = $('.dot').first();
+        }
+        
+        currentSlide.fadeOut(0).removeClass('active-slide');
+        nextSlide.fadeIn(0).addClass('active-slide');
+        
+        currentDot.removeClass('active-dot');
+        nextDot.addClass('active-dot');
+    });
+    
+    $('.arrow-prev').click(function () {
+        var currentDot = $('.active-dot');
+        var currentSlide = $('.active-slide');
+        var prevDot = currentDot.prev();
+        var prevSlide = currentSlide.prev();
+        
+        if (prevSlide.length == 0) {
+            prevSlide = $('.slide').last();
+            prevDot = $('.dot').last();
+        }
+                
+        currentSlide.fadeOut(0).removeClass('active-slide');
+        prevSlide.fadeIn(0).addClass('active-slide');
+        
+        currentDot.removeClass('active-dot');
+        prevDot.addClass('active-dot');
+    });
 };
 
 $(document).ready(main);
